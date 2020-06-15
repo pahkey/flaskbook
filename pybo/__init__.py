83a7b7e0b510a166bf4c3e3c9f74b875f9d88195
박응용 <pahkey@gmail.com>
2020-06-15 12:18:15 +0900

    2-03

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,8 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_pybo():
-        return 'Hello, Pybo!'
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
 
     return app
---
+++ pybo/views/main_views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return 'Pybo index'
+
