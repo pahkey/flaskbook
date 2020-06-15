b5b724090e7c7993fc78e12b02672a255f9c7e4e
박응용 <pahkey@gmail.com>
2020-06-15 14:28:26 +0900

    3-02

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -12,7 +12,9 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 
