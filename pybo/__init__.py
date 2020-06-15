fdb2f485838c3abd10b4daf55a1618e24ac10b13
박응용 <pahkey@gmail.com>
2020-06-15 14:35:18 +0900

    3-03

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -23,4 +23,8 @@ def create_app():
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
 
+    # 필터
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
+
     return app
---
+++ pybo/filter.py
@@ -0,0 +1,2 @@
+def format_datetime(value, fmt='%Y년 %m월 %d일 %H:%M'):
+    return value.strftime(fmt)
